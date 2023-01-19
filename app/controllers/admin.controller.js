68653a74a47753c58e7bfb62ef2b7913537b9fa2
Rif'an Fatoni Febrianto <73413289+Rifaaanf@users.noreply.github.com>
2023-01-19 12:11:10 +0700

    crud for data in db

--- app/controllers/admin.controller.js
+++ app/controllers/admin.controller.js
@@ -76,3 +76,151 @@ exports.changestodata = (req, res) => {
     }
   );
 };
+
+exports.changesegmendata = (req, res) => {
+  // change segmen data name to new name
+  segmen.findOneAndUpdate(
+    { name: req.body.segmen },
+    { name: req.body.segmenbaru },
+    { new: true },
+    (err, data) => {
+      if (err) {
+        res.status(500).send({ message: err });
+        return;
+      }
+      res.status(200).send(data);
+    }
+  );
+};
+
+exports.changejenisqedata = (req, res) => {
+  // change jenisQE data name to new name
+  jenisQE.findOneAndUpdate(
+    { name: req.body.jenisQE },
+    { name: req.body.jenisQEbaru },
+    { new: true },
+    (err, data) => {
+      if (err) {
+        res.status(500).send({ message: err });
+        return;
+      }
+      res.status(200).send(data);
+    }
+  );
+};
+
+exports.changenamaalprodata = (req, res) => {
+  // change namaAlpro data name to new name
+  namaAlpro.findOneAndUpdate(
+    { name: req.body.namaAlpro },
+    { name: req.body.namaAlprobaru },
+    { new: true },
+    (err, data) => {
+      if (err) {
+        res.status(500).send({ message: err });
+        return;
+      }
+      res.status(200).send(data);
+    }
+  );
+};
+
+exports.addnamasto = (req, res) => {
+  // add new namaSTO
+  const namasto = new namaSTO({
+    name: req.body.namaSTO,
+  });
+  namasto.save((err, data) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    res.status(200).send(data);
+  });
+};
+
+exports.addsegmen = (req, res) => {
+  // add new segmen
+  const segmen = new segmen({
+    name: req.body.segmen,
+  });
+  segmen.save((err, data) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    res.status(200).send(data);
+  });
+};
+
+exports.addjenisqe = (req, res) => {
+  // add new jenisQE
+  const jenisqe = new jenisQE({
+    name: req.body.jenisQE,
+  });
+  jenisqe.save((err, data) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    res.status(200).send(data);
+  });
+};
+
+exports.addnamaalpro = (req, res) => {
+  // add new namaAlpro
+  const namaalpro = new namaAlpro({
+    name: req.body.namaAlpro,
+  });
+  namaalpro.save((err, data) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    res.status(200).send(data);
+  });
+};
+
+exports.deletenamasto = (req, res) => {
+  // delete namaSTO
+  namaSTO.findOneAndDelete({ name: req.body.namaSTO }, (err, data) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    res.status(200).send(data);
+  });
+};
+
+exports.deletesegmen = (req, res) => {
+  // delete segmen
+  segmen.findOneAndDelete({ name: req.body.segmen }, (err, data) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    res.status(200).send(data);
+  });
+};
+
+exports.deletejenisqe = (req, res) => {
+  // delete jenisQE
+  jenisQE.findOneAndDelete({ name: req.body.jenisQE }, (err, data) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    res.status(200).send(data);
+  });
+};
+
+exports.deletenamaalpro = (req, res) => {
+  // delete namaAlpro
+  namaAlpro.findOneAndDelete({ name: req.body.namaAlpro }, (err, data) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    res.status(200).send(data);
+  });
+};
--- app/routes/admin.routes.js
+++ app/routes/admin.routes.js
@@ -20,5 +20,75 @@ module.exports = function (app) {
 
   app.get("/detail/:id", [authJwt.verifyToken], controller.detailProposal);
 
-  app.post("/changedata/sto", controller.changestodata);
+  app.post(
+    "/changedata/namasto",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.changestodata
+  );
+
+  app.post(
+    "/changedata/namaalpro",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.changenamaalprodata
+  );
+
+  app.post(
+    "/changedata/segmen",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.changesegmendata
+  );
+
+  app.post(
+    "/changedata/jenisqe",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.changejenisqedata
+  );
+
+  app.post(
+    "/adddata/namasto",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.addnamasto
+  );
+
+  app.post(
+    "/adddata/alpro",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.addnamaalpro
+  );
+
+  app.post(
+    "/adddata/segmen",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.addsegmen
+  );
+
+  app.post(
+    "/adddata/jenisqe",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.addjenisqe
+  );
+
+  app.post(
+    "/deletedata/namasto",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.deletenamasto
+  );
+
+  app.post(
+    "/deletedata/namaalpro",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.deletenamaalpro
+  );
+
+  app.post(
+    "/deletedata/segmen",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.deletesegmen
+  );
+
+  app.post(
+    "/deletedata/jenisqe",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.deletejenisqe
+  );
 };
